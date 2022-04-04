4b38c010db50523d638555c33962468e857bf85b
mdataurrahman555 <74402921+mdataurrahman555@users.noreply.github.com>
2022-04-04 16:51:23 +0600

    added notFound and _redirect

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='not_found'></div>
   )
 }
 
